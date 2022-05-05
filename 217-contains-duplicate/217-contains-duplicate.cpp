4bb627ce62c7527041e0305c4be7edfe99097388
Khushboo Shakya <72929479+khushbooshakya@users.noreply.github.com>
2022-05-05 22:30:04 +0530

    Time: 122 ms (51.59%), Space: 55.3 MB (6.39%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+         unordered_set<int> s{nums.begin(), nums.end()};
+        return s.size() != nums.size();
+        
+    }
+};
+//agar no of element 4 hai nd ushme index 3 tk hi like 1,2,3,1 (ishme 1,2,3 tk hi hai ,4 nhi hai ) to bo array mai atleast ek na ek element repeat kr ra hoga
