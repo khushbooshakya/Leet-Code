16d4492c2006245033488a03e31ea197247a43ed
Khushboo Shakya <72929479+khushbooshakya@users.noreply.github.com>
2022-06-07 09:51:21 +0530

    Time: 113 ms (12.80%), Space: 14.6 MB (72.37%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // Initializing the two pointers
+        ListNode *ptr1 = headA;
+        ListNode *ptr2 = headB;
+        
+        while(ptr1 != ptr2){
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+            
+            // break the loop if they reach the end together or collide
+            if(ptr1 == ptr2) break;
+            
+            // if the pointer reaches the end of A, go to B and vice versa for ptr2
+            if(ptr1 == NULL) ptr1 = headB;
+            if(ptr2 == NULL) ptr2 = headA;
+ 
+        }
+        // return the collided pointer
+        return ptr1;
+    }
+};
