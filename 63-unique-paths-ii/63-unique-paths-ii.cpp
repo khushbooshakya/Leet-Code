d4a779e15167a883d2b2a25e80f3e36196740832
Khushboo Shakya <72929479+khushbooshakya@users.noreply.github.com>
2022-06-29 10:09:14 +0530

    Time: 4 ms (64.01%), Space: 7.6 MB (90.31%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,13 +1,55 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
-        int m = obstacleGrid.size() , n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        dp[0][1] = 1;
-        for(int i = 1 ; i <= m ; ++i)
-            for(int j = 1 ; j <= n ; ++j)
-                if(!obstacleGrid[i-1][j-1])
-                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
-        return dp[m][n];
-    }
+    int ct = 0; 
+    
+    vector<pair<int, int> > movements = {
+        {0, 1}, {1,0}
     };
+    bool isvalid(int i, int j, int m, int n)
+    {
+        return i>=0 && j>=0 && i<m && j<n;
+    }
+    
+    int dp[150][150];
+    
+    void generate(int i, int j, int m, int n, vector<vector<int>> &obstacleGrid)
+    {
+        int ct1 = ct;
+        if(dp[i][j] != -1)
+        {
+            ct += dp[i][j];
+            return;
+        }
+        if(i == m-1 && j == n-1)
+        {
+            ct += 1;
+            return;
+        }
+               
+        for(auto movement: movements)
+        {
+            int newi = i + movement.first;
+            int newj = j + movement.second;
+            
+            if(!isvalid(newi, newj, m, n)) continue;
+            if(obstacleGrid[newi][newj] == 1) continue;
+            generate(newi, newj, m, n, obstacleGrid);        
+        }
+   
+        dp[i][j] = ct-ct1;
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
+    {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        
+        memset(dp, -1, sizeof(dp));
+        
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+        
+        generate(0, 0, m ,n, obstacleGrid);
+        return ct;
+        
+    }
+};
