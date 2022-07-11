6609840d06e418520122af4c28e9f5a80dd0ac26
Khushboo Shakya <72929479+khushbooshakya@users.noreply.github.com>
2022-07-11 08:27:18 +0530

    Added solution - LeetHub

---
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -0,0 +1,95 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function template for C++
+class Solution 
+{
+  void solve(int i, int j, vector < vector < int >> & a, int n, vector < string > & ans, string move,
+            vector < vector < int >> & vis, int di[], int dj[]) 
+    {
+        // Boundary cases
+        if (i == n - 1 && j == n - 1) 
+        {
+            ans.push_back(move);
+            return;
+        }
+        
+        // Store the 4 direction in a string variable
+        string dir = "DLRU";
+        
+        // Iterate from 0 to 4 
+        for (int ind = 0; ind < 4; ind++) 
+        {
+            // Compute the nexti and nextj
+            int nexti = i + di[ind];
+            int nextj = j + dj[ind];
+            
+            // Check all the boundary cases and then amke a move
+            if (nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && !vis[nexti][nextj] && a[nexti][nextj] == 1) 
+            {
+                // Mark the visited array as 1
+                vis[i][j] = 1;
+                
+                // Go for the next recursion call
+                solve(nexti, nextj, a, n, ans, move + dir[ind], vis, di, dj);
+                
+                // Backtracking step 
+                vis[i][j] = 0;
+            
+            }
+        }
+
+  }
+  public:
+    vector < string > findPath(vector < vector < int >> & m, int n) 
+    {
+        // Define the ans vector
+        vector < string > ans;
+        
+        // Make the visited array
+        vector < vector < int >> vis(n, vector < int > (n, 0));
+        
+        // Define the 4 direction
+        int di[] = {+1,0,0,-1};
+        int dj[] = {0,-1,1,0};
+      
+        // Check for statring pos if it's 1 or not
+        if (m[0][0] == 1) solve(0, 0, m, n, ans, "", vis, di, dj);
+        
+        // Fianlly return the ans
+        return ans;
+    }
+};
+    
+
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
