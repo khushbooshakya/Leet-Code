cf1381bc3fd22a2b6e5181af75fe8e51bab5d69b
Khushboo Shakya <72929479+khushbooshakya@users.noreply.github.com>
2022-07-25 17:56:25 +0530

    Time: 15 ms (39.90%), Space: 13.6 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,58 @@
 class Solution {
+     int firstpos(vector<int>& nums,int target){
+         if(nums.size()==0){
+                return -1;
+            }
+            int ans=-1;
+            int lo=0,hi=nums.size()-1;
+            while(lo<=hi){
+                    int mid=lo+(hi-lo)/2;
+                    if(nums[mid]==target){
+                        ans=mid;
+                        hi=mid-1;
+                    }
+                    else if(nums[mid]>=target){
+                         hi= mid-1;
+                      
+                    }
+                    else{
+                       lo= mid+1;
+                    }
+            }
+            return ans;
+        }
+    int lastpos(vector<int>& nums,int target){
+            if(nums.size()==0){
+                return -1;
+            }
+            
+            int ans=-1;
+            int lo=0,hi=nums.size()-1 ;
+            
+            while(lo<=hi){
+                int mid=lo+(hi-lo)/2;
+                    if(nums[mid]==target){
+                        ans=mid;
+                        lo=mid+1;
+                        
+                    }
+                    else if(nums[mid]<=target){
+                      lo=  mid+1;
+                    }
+                    else{
+                       hi= mid-1;
+                    }
+                }
+            return ans;
+    }
+   
 public:
- int firstindex(vector<int>nums,int target)
-    {
-        if(nums.size()==0)
-            return -1;
-        int i=0;
-        int j=nums.size()-1;
-        while(i<=j)
-        {
-            int mid = i+(j-i)/2;
-            if(nums[mid]>=target)
-            {
-                j=mid-1;
-            }
-            else
-                i = mid+1;
-        }
-        if(i<nums.size() && i>=0 && nums[i]==target)
-            return i;
-        return -1;
-    }
-    int lastindex(vector<int>nums,int target)
-    {
-        if(nums.size()==0)
-            return -1;
-        int i=0;
-        int j=nums.size()-1;
-        while(i<=j)
-        {
-            int mid = i+(j-i)/2;
-            if(nums[mid]<=target)
-            {
-                i=mid+1;
-            }
-            else
-                j = mid-1;
-        }
-        if(j>=0 && j<nums.size() && nums[j]==target)
-            return j;
-        return -1;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>vec;
-        vec.push_back(firstindex(nums,target));
-        vec.push_back(lastindex(nums,target));
-        return vec;
-    }
+        vector<int>ans(2);
+        ans[0] = firstpos(nums , target);
+        ans[1] = lastpos(nums , target);
+        return ans;
 
+    }
 };
+
