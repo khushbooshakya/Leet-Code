5a268db024a8cd8925e3e9685a9139d98cc64a0e
Khushboo Shakya <72929479+khushbooshakya@users.noreply.github.com>
2022-08-21 20:42:22 +0530

    Time: 18 ms (40.47%), Space: 7.1 MB (94.49%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // if length is not same , they can't be anagram.
+        if (s.size()!=t.size()){
+            return false;
+       }
       sort(s.begin(),s.end());
       sort(t.begin(),t.end());
-return s==t;
+          return s==t;
     }
 };
